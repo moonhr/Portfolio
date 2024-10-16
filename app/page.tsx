da9267f2b9b2708551428242c4aa8bf03d361e29
moonhr <moonhr153@gmail.com>
2024-10-16 18:04:15 +0900

    pages수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,10 @@
 import React from "react";
-import Card from "../src/components/card";
+import DashboardLayout from "../src/components/templates/DashboardLayout";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-4 w-full h-full">
-      <Card>as</Card>
-      <Card>as</Card>
-      <Card>as</Card>
-      <Card>as</Card>
-      <Card>as</Card>
-      <Card>as</Card>
-      <Card>as</Card>
-      <Card>as</Card>
-      <Card>as</Card>
-      <Card>as</Card>
-      <Card>as</Card>
+    <div>
+      <DashboardLayout />
     </div>
   );
 }
