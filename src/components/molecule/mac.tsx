fb2946920ac78617ecfb2502e966b0bf800694d4
moonhr <moonhr153@gmail.com>
2024-10-16 18:05:12 +0900

    추가 맥 목업

---
+++ src/components/molecule/mac.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+interface MacProps {
+  src: string;
+  alt: string;
+  width: number;
+  height: number;
+}
+
+const Mac = ({ src, alt, width, height }: MacProps) => {
+  return (
+    <div className="relative z-10 w-full">
+      <Image
+        src="/mac.png"
+        alt="mac"
+        width={200}
+        height={200}
+        className="z-10 relative w-full"
+      />
+      <div className="absolute top-0 w-full flex justify-center items-center mt-1">
+        <Image
+          src={src}
+          alt={alt}
+          width={width}
+          height={height}
+          className="z-0 absolute top-0 w-5/6 m-auto"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Mac;
