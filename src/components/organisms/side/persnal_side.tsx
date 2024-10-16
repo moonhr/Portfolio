ea757e26ac16300a788bf8ea20414eade5e6139f
moonhr <moonhr153@gmail.com>
2024-10-16 18:59:46 +0900

    수정 | qr데이터 있다면 추가

--- src/components/organisms/side/persnal_side.tsx
+++ src/components/organisms/side/persnal_side.tsx
@@ -1,19 +1,35 @@
 import Title from "../../atom/title";
 import Stack from "../../molecule/stack";
 import Caption from "../../atom/caption";
-
+import QrImg from "../../molecule/qr_img";
 interface PersnalSideProps {
   title: string;
   stack: {
     [key: string]: string[];
   };
   caption: string;
+  qr?: {
+    src?: string;
+    alt?: string;
+    link?: string;
+  };
 }
 
-const PersnalSide = ({ title, stack, caption }: PersnalSideProps) => {
+const PersnalSide = ({ title, stack, caption, qr }: PersnalSideProps) => {
   return (
     <div className="flex flex-col justify-between h-full gap-4">
       <Title text={title} />
+      {qr && qr.src && (
+        <div className="relative group">
+          <QrImg
+            src={qr.src}
+            alt={qr.alt || "QR 코드"}
+            width={100}
+            height={100}
+            link={qr.link}
+          />
+        </div>
+      )}
       <Stack stack={stack} />
       <Caption text={caption} />
     </div>
