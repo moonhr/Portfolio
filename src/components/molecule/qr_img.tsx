bf963792737a1e9a60e7c26e7fa64cfe5eb427e3
moonhr <moonhr153@gmail.com>
2024-10-16 18:58:59 +0900

    수정 | qr 클릭시 호버효과, 링크이동

--- src/components/molecule/qr_img.tsx
+++ src/components/molecule/qr_img.tsx
@@ -1,17 +1,30 @@
-import Img from "../atom/img";
+import Image from "next/image";
 
-const QrImg = ({
-  src,
-  alt,
-  width,
-  height,
-}: {
+interface QrImgProps {
   src: string;
   alt: string;
   width: number;
   height: number;
-}) => {
-  return <Img src={src} alt={alt} width={width} height={height} />;
+  link?: string;
+}
+
+const QrImg: React.FC<QrImgProps> = ({ src, alt, width, height, link }) => {
+  return (
+    <div className="relative group hover:cursor-pointer hover:opacity-80">
+      <a href={link || "#"} target="_blank" rel="noopener noreferrer">
+        <Image
+          src={src}
+          alt={alt}
+          width={width}
+          height={height}
+          className="transition-transform duration-300 ease-in-out transform group-hover:scale-110"
+        />
+        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
+          <span className="text-white text-sm font-bold">사이트로 이동</span>
+        </div>
+      </a>
+    </div>
+  );
 };
 
 export default QrImg;
