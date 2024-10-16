550719e6e10356e9ea478b9ff520328a23019640
moonhr <moonhr153@gmail.com>
2024-10-16 18:05:29 +0900

    추가 이름 카드

---
+++ src/components/molecule/NameCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "../utils/Card";
+
+const NameCard = () => {
+  return (
+    <Card>
+      <div>문혜림</div>
+    </Card>
+  );
+};
+
+export default NameCard;
